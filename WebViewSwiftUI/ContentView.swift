3a142d13508381cd09892f85ee4653300a268861
Fabrício Masiero <masiero572@gmail.com>
2020-03-09 17:13:12 -0300

    Initial Commit

---
+++ WebViewSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebViewSwiftUI
+//
+//  Created by Fabrício Masiero on 09/03/20.
+//  Copyright © 2020 Fabrício Masiero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
